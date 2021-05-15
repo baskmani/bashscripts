8506d97d0dc70861767211be1c22c49e0e340cf9
baskmani <83446788+baskmani@users.noreply.github.com>
2021-05-15 10:30:12 +0530

    This script is to find the active EKS clusters in the AWS account

--- eks.sh
+++ eks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-`aws ec2 describe-regions --region us-east-1 --output text | cut -f4 > region.text`
+`aws ec2 describe-regions --region us-east-1 --output text | cut -f4 > region.txt`
  region_count=$(cat /Users/baskmani/Desktop/region.text|wc -l)
 
 for (( i=1; i<=$region_count; i++ ))
     do
-        region=$(sed -n $i'p' region.text)
+        region=$(sed -n $i'p' region.txt)
         aws eks list-clusters --region $region --output text
     done
 
-rm -rf region.text
+rm -rf region.txt
 
