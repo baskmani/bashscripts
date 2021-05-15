e4baa001bcf50a4641fdcf49c06b4e9a4fc21fa6
baskmani <83446788+baskmani@users.noreply.github.com>
2021-05-15 14:23:19 +0530

    Update eks.sh

--- eks.sh
+++ eks.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
-`aws ec2 describe-regions --region us-east-1 --output text | cut -f4 > region.txt`
- region_count=$(cat /Users/baskmani/Desktop/region.text|wc -l)
-
+`aws ec2 describe-regions --region us-east-1 --output text | cut -f4 > region.text`
+region_count=$(cat region.text|wc -l)
 for (( i=1; i<=$region_count; i++ ))
-    do
-        region=$(sed -n $i'p' region.txt)
+do
+    region=$(sed -n $i'p' region.text)
+    aws eks list-clusters --region $region --output text | grep - clu > /dev/null 2>&1
+    if [ $? == 0 ]
+    then
+        echo " "
+        echo -e "The cluster is in the region: \e[1;32m$region\e[0m"
         aws eks list-clusters --region $region --output text
-    done
-
-rm -rf region.txt
+        echo " "
+    fi
+done
 
